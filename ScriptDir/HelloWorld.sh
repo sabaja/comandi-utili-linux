7ab1b46e6ce394917f941fa6858f807ec7632a35
sabaja <jacoposabatini76@gmail.com>
2018-11-27 23:08:48 +0100

    afgggiunta roba

---
+++ ScriptDir/HelloWorld.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#comment
+echo La prima riga informa il sistema che il file deve essere eseguito da /bin/bash, che corrisponde al percorso standard della Bourne-Again Shell su qualsiasi sistema Unix based
